7842aa19adb2d324f21568550bf0299aca99b119
Andrei Scafi <andreiscafi@hotmail.com>
2024-02-07 06:53:07 -0300

    Adding event listeners and checking if the input has a valid number with javascript.

--- script.js
+++ script.js
@@ -3,3 +3,23 @@ const numberInput = document.getElementById('number-input');
 const convertBtn = document.getElementById('convert-btn');
 const result = document.getElementById('result');
 // End of Variables
+
+// Check User Input
+function checkUserInput() {
+    if (!numberInput.value || isNaN(parseInt(numberInput.value))) {
+        alert('Please provide a decimal number.');
+        return;
+    };
+    console.log(numberInput.value);
+};
+// End of Check User Input
+
+//Event Listeners
+convertBtn.addEventListener('click', checkUserInput);
+
+numberInput.addEventListener('keydown', (e) => {
+    if (e.key === 'Enter') {
+        checkUserInput()
+    };
+});
+//End of Event Listeners
