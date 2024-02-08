090a057e33c1985b1729eaf451cbd5fcea8974e6
Andrei Scafi <andreiscafi@hotmail.com>
2024-02-08 09:18:39 -0300

    Refactoring decimal to binary function with javascript.

--- script.js
+++ script.js
@@ -7,31 +7,48 @@ const result = document.getElementById('result');
 // Decimal to Binary converter function
 function decimalToBinary(input) {
 
-    const inputs = [];
-    const quotients = [];
-    const remainders = [];
+    /*   Simple solution but not efficiently
+      const inputs = [];
+      const quotients = [];
+      const remainders = [];
+  
+      if (input === 0) {
+          result.innerText = '0';
+          return;
+      };
+  
+      while (input > 0) {
+  
+          const quotient = Math.floor(input / 2);
+          const remainder = input % 2;
+  
+          inputs.push(input);
+          quotients.push(quotient);
+          remainders.push(remainder);
+  
+          input = quotient;
+      };
+      console.log('Inputs: ', inputs);
+      console.log('Quotients: ', quotients);
+      console.log('Remainders: ', remainders);
+  
+      result.innerText = remainders.reverse().join(''); 
+      */
+
+    //New solution
+
+    let binary = '';
 
     if (input === 0) {
-        result.innerText = '0';
-        return;
+        binary = '0'
     };
 
     while (input > 0) {
-
-        const quotient = Math.floor(input / 2);
-        const remainder = input % 2;
-
-        inputs.push(input);
-        quotients.push(quotient);
-        remainders.push(remainder);
-
-        input = quotient;
+        binary = (input % 2) + binary;
+        input = Math.floor(input / 2);
     };
-    console.log('Inputs: ', inputs);
-    console.log('Quotients: ', quotients);
-    console.log('Remainders: ', remainders);
 
-    result.innerText = remainders.reverse().join('');
+    result.innerText = binary;
 };
 
 // End of Decimal to Binary converter function
