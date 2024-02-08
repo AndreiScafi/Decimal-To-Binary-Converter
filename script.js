28ccf8c742ef3c779d6d34bf252a74143857e5ea
Andrei Scafi <andreiscafi@hotmail.com>
2024-02-08 07:51:17 -0300

    Adding Decimal to Binary converter function with Javascript part 1.

--- script.js
+++ script.js
@@ -4,13 +4,41 @@ const convertBtn = document.getElementById('convert-btn');
 const result = document.getElementById('result');
 // End of Variables
 
+// Decimal to Binary converter function
+function decimalToBinary(input) {
+
+    const inputs = [];
+    const quotients = [];
+    const remainders = [];
+
+
+
+    while (input > 0) {
+
+        const quotient = Math.floor(input / 2);
+        const remainder = input % 2;
+
+        inputs.push(input);
+        quotients.push(quotient);
+        remainders.push(remainder);
+
+        input = quotient;
+    };
+    console.log('Inputs: ', inputs);
+    console.log('Quotients: ', quotients);
+    console.log('Remainders: ', remainders);
+};
+
+// End of Decimal to Binary converter function
+
 // Check User Input
 function checkUserInput() {
     if (!numberInput.value || isNaN(parseInt(numberInput.value))) {
         alert('Please provide a decimal number.');
         return;
     };
-    console.log(numberInput.value);
+    decimalToBinary(parseInt(numberInput.value));
+    numberInput.value = '';
 };
 // End of Check User Input
 
